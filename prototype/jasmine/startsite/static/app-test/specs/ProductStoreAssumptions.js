81c5a78875ebc9cb2f6c31180c515b79060662a6
felipeabgr <felipe.douglas@inatel.br>
2014-04-25 09:16:57 -0300

    novo arquivo de teste ViewProductNewAssumptions

--- prototype/jasmine/startsite/static/app-test/specs/ProductStoreAssumptions.js
+++ prototype/jasmine/startsite/static/app-test/specs/ProductStoreAssumptions.js
@@ -7,36 +7,36 @@ Ext.define('Test.spec.ProductStoreAssumptions', {}, function() {
     });
 
     describe('Prod.store.Products Store', function() {
-        it('It has the store loaded', function() {
-            /*var prodStore = Ext.create('Ext.data.Store', {
-                 autoLoad: true,
-                 model: "Prod.model.Product",
-                 proxy: {
-                    type: 'ajax',
-                    url: '/static/data/products.json',
-
-                    reader: {
-                        type: 'json',
-                        root: 'products',
-                        successProperty: 'success'
-                    }
-
-                }
-            });*/
+        it('It Exists',function() {
             var prodStore = Ext.create('Prod.store.Products');
-            // prodStore.load({
-            //     id: 0,
-            //     scope:this,
-            //     callback: function(records, operation, success){
-            //         if(success){
-            //           var prod = records[0];
-            //         }
-            //     }
-            // });
+            expect(prodStore.$className).toEqual('Prod.store.Products');
+        });
 
-            //expect(prodStore.getCount()).toBe(3);
+        it('It has the store loaded', function() {
+            var prodStore = Ext.create('Prod.store.Products');
+            prodStore.load();
+            expect(prodStore.getCount()).toBeGreaterThan(0);
+
             expect(true).toBe(true);
         });
+
+        it('1st Element', function() {
+            var contact = null;
+            var prodStore = Ext.create('Prod.store.Products');
+            prodStore.load({
+                id: 1,
+                scope:this,
+                callback: function(records, operation, success){
+                    if(success){
+                        contact = records[0];
+                        expect(contact.data.id).toEqual(1);
+                    }
+                }
+            });
+            waits(1000);
+            expect(contact.data.id).toEqual(1);
+        });
+
 
     });
 
