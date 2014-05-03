0def0c950bba7388b482740468814a9d5fdc6515
leandromaia <leandroc@inatel.br>
2014-05-03 02:19:47 -0300

    Criação de testes para authenticate do django

--- sodexoapp/access/tests.py
+++ sodexoapp/access/tests.py
@@ -1,12 +1,15 @@
 #coding: utf-8
+import mock
+
 from django.test.client import Client
 from django.test import TestCase
+from django.contrib.auth import authenticate
 
 
 class AccessAuthorizationDjangoTest(TestCase):
     fixtures = ['basic_auth.yaml']
 
-    def test_access_login_page(self):
+    def test_access_login_by_domain(self):
         c = Client()
         ret = c.get('/', follow=True)
         self.assertEquals(ret.status_code, 200, 'Wrong status code.')
@@ -21,14 +24,26 @@ class AccessAuthorizationDjangoTest(TestCase):
         ret = c.get('/access/logout', follow=True)
         self.assertEquals(ret.status_code, 200, 'Wrong status code.')
 
-    def test_login_allowed(self):
+    def test_login_authorized(self):
         c = Client()
-        ret = c.post('/access/login', {'username': 'admin', 'password': 'admin'})
+        ret = c.post('/access/login', {'username': 'admin', 'password': 'admin'}, follow=True)
         self.assertEquals(ret.status_code, 200, 'Wrong status code.')
+        self.assertFalse('error_msg' in ret.context)
 
-    def test_login_not_allowed(self):
+    def test_login_not_authorized(self):
         c = Client()
         ret = c.post('/access/login', {'username': 'admin', 'password': 'ssss'})
 
         self.assertEquals(ret.status_code, 200, 'Wrong status code.')
+        self.assertTrue('error_msg' in ret.context)
+
+    @mock.patch.object(access.views.authenticate)
+    def test_login_django_autenticate_mock(self, m_authenticate):
+
+        m_authenticate.return_value = None
+
+        c = Client()
+        ret = c.post('/access/login', {'username': 'admin', 'password': 'admin'},
+         follow=True)
+
         self.assertTrue('error_msg' in ret.context)
