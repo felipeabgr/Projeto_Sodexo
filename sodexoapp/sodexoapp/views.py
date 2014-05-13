92ab506fa8bab2d0545b62256f17b1bcb4bdc344
felipeabgr <felipe.douglas@inatel.br>
2014-05-13 16:09:55 -0300

    inclusão do arquivo views.py em sodexoapp

--- sodexoapp/sodexoapp/views.py
+++ sodexoapp/sodexoapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
 def recover_password(request):
-    # return render(request, 'websodexoapp/app/view/NewPassword.js', content_type='text/javascript')
-    # return render(request, 'index2.html', content_type='text/html')
+    return render(request, 'pwrecover.html', content_type='text/html')
