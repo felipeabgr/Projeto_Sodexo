891bb1e867c4bfd5596d26c430232721e5912be4
Tiagohl <tiagoh@inatel.br>
2014-05-15 14:05:44 -0300

    correções no teste NewPssword controller e pep8

--- sodexoapp/websodexoapp/app-test/specs/NewPasswordControllerSpec.js
+++ sodexoapp/websodexoapp/app-test/specs/NewPasswordControllerSpec.js
@@ -1,4 +1,4 @@
-describe('Controller User', function() {
+describe('Controller NewPassword', function() {
     var ctr;
     var view;
 
@@ -29,59 +29,23 @@ describe('Controller User', function() {
         ctr.sendEmail();
         expect(ctr.sendEmail).toHaveBeenCalled();
     });
-});
 
-describe('Mocking Ajax controller calls', function() {
+    describe('Mocking Ajax controller calls', function() {
 
-    beforeEach(function() {
-        jasmine.Ajax.install();
-    });
+        beforeEach(function() {
+            jasmine.Ajax.install();
+        });
 
-    afterEach(function() {
-        jasmine.Ajax.uninstall();
-    });
+        afterEach(function() {
+            jasmine.Ajax.uninstall();
+        });
 
-    it('should make an AJAX request to the correct URL', function() {
-        debugger;
-        var controller = Ext.create('Sodexoapp.controller.access.NewPassword');
-        controller.defineNewPassword(1);
-
-        expect(jasmine.Ajax.requests.mostRecent().url).toContain('/access/userauthentication/1');
-    });
-
-    it('makes a Ajax request to recover the user password', function() {
+        it('should make an AJAX request to the correct URL', function() {
             var controller = Ext.create('Sodexoapp.controller.access.NewPassword');
             controller.defineNewPassword(1);
 
-            var countFn = jasmine.createSpy("success");
-
-            var store = Ext.create('Sodexoapp.store.access.Users');
-
-            store.on('load', function(store){
-                countFn(store.data.length);
-            });
-
-            store.load();
-
-            var mockedRequest = jasmine.Ajax.requests.mostRecent();
-
-            mockedRequest.response({
-                status:       200,
-                responseText: "{success: 'true',"+
-                               "users: ["+
-                                   "{"+
-                                       "id: '1',"+
-                                       "username: 'zoio',"+
-                                       "email: 'leandroc@inatel.br',"+
-                                       "password: 'svcfasasdasd'},"+
-                                   "{"+
-                                       "id: '2',"+
-                                       "username: 'corvo',"+
-                                       "email: 'felipe.douglas@inatel.br',"+
-                                       "password: 'fkfkfkfkfkfkfk'}"+
-                                "]}"
-            });
-
-            expect(countFn).toHaveBeenCalledWith(2);
+            expect(jasmine.Ajax.requests.mostRecent().url).toContain('/access/userauthentication/1');
         });
+
+    });
 });
