ca56501f8db8c70e690cd95a211754af6ccb7ee5
leandromaia <leandroc@inatel.br>
2014-05-16 15:22:33 -0300

    Criação do primeiro testes da store balance

--- sodexoapp/websodexoapp/app-test/specs/balance/BalanceStoreSpec.js
+++ sodexoapp/websodexoapp/app-test/specs/balance/BalanceStoreSpec.js
@@ -1,1 +1,7 @@
-//Jasmine Tests
+describe('Balance Store', function() {
+
+    it('should exists',function() {
+        var userStore = Ext.create('Sodexoapp.store.balance.Balances');
+        expect(userStore.$className).toEqual('Sodexoapp.store.balance.Balances');
+    });
+});
