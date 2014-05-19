ad68fea0c571a07415f3eaeb99013e8d29f15337
Tiagohl <tiagoh@inatel.br>
2014-05-19 10:16:04 -0300

    correção dos valores para teste dos fields do tipo float

--- sodexoapp/websodexoapp/app-test/specs/consultation/BalanceModelSpec.js
+++ sodexoapp/websodexoapp/app-test/specs/consultation/BalanceModelSpec.js
@@ -8,17 +8,17 @@ describe('Model Balance', function(){
     it('has properties', function() {
         var model = Ext.create('Sodexoapp.model.consultation.Balance', {
             date:'03/13/2014',
-            balance:'123456',
-            daily_value:'20',
+            balance:'1234.56',
+            daily_value:'20.2',
             remaining_days:'5',
-            leftover:'100'
+            leftover:'100.2'
 
         });
         expect(model.get('date')).toEqual(new Date('03/13/2014'));
-        expect(model.get('balance')).toEqual(123456);
-        expect(model.get('daily_value')).toEqual(20);
+        expect(model.get('balance')).toEqual(1234.56);
+        expect(model.get('daily_value')).toEqual(20.2);
         expect(model.get('remaining_days')).toEqual(5);
-        expect(model.get('leftover')).toEqual(100);
+        expect(model.get('leftover')).toEqual(100.2);
     });
 
 });
