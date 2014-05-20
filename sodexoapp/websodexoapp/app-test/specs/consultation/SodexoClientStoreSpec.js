8d8490834b290712137c8077060dd1dc3f0d9179
leandromaia <leandroc@inatel.br>
2014-05-20 16:20:52 -0300

    Correção na store

--- sodexoapp/websodexoapp/app-test/specs/consultation/SodexoClientStoreSpec.js
+++ sodexoapp/websodexoapp/app-test/specs/consultation/SodexoClientStoreSpec.js
@@ -30,6 +30,7 @@ describe('Store SodexoClient', function() {
             var store = Ext.create('Sodexoapp.store.consultation.SodexoClients');
 
             store.on('load', function(store){
+                debugger;
                 var sodexoclientFake = store.data.items[0].data;
                 idDm(sodexoclientFake.id);
                 cpfDm(sodexoclientFake.username);
--- sodexoapp/websodexoapp/app/store/consultation/SodexoClients.js
+++ sodexoapp/websodexoapp/app/store/consultation/SodexoClients.js
@@ -1,6 +1,6 @@
 Ext.define('Sodexoapp.store.consultation.SodexoClients',{
     extend:'Ext.data.Store',
-    //model: 'Sodexoapp.model.consultation.SodexoClients',
+    model: 'Sodexoapp.model.consultation.SodexoClients',
     proxy: {
         type: 'rest',
         url: '/consultation/sodexoclient',
