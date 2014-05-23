28ea1acb3ea5354445e5bf581186efad4f8912e1
Tiagohl <tiagoh@inatel.br>
2014-05-23 10:47:58 -0300

    Requisição ajax para para retornar as informações do saldo

--- sodexoapp/websodexoapp/app/controller/consultation/Balance.js
+++ sodexoapp/websodexoapp/app/controller/consultation/Balance.js
@@ -4,4 +4,38 @@ Ext.define('Sodexoapp.controller.consultation.Balance', {
     views: [
         'consultation.Balance'
     ],
+
+    refs: [
+        {ref: 'balanceView', selector: 'balaceConsult'},
+        {ref: 'captchField', selector: 'balaceConsult #captchaField'}
+    ],
+
+    init: function(){
+         this.control({
+            'balaceConsult button[itemId=sendCaptcha]': {
+                click: this.buildPost
+            }
+        });
+    },
+
+    buildPost: function(button){
+        var captchText = this.getCaptchField().getValue();
+        var user = Sodexoapp.Session.logged_user;
+        Ext.Ajax.request({
+            url: '/consultation/balance',
+            method: 'POST',
+            jsonData: {
+                "user_id" : user.id,
+                "captch_text" : captchText,
+            },
+            success: function(response){
+                debugger;
+                var result = response.responseText;
+            },
+            failure: function(response){
+                debugger;
+                var teste = response;
+            }
+        });
+    }
 });
--- sodexoapp/websodexoapp/app/view/consultation/Balance.js
+++ sodexoapp/websodexoapp/app/view/consultation/Balance.js
@@ -29,19 +29,28 @@ Ext.define('Sodexoapp.view.consultation.Balance', {
                         }
                     },
                     {
-                        xtype: 'panel',
+                        xtype: 'component',
                         header: false,
                         itemId: 'captchaBox',
                         height: 80,
                         width: 200,
-                        margin: "10 0 10 0"
+                        margin: "10 0 10 0",
+                        autoEl: {
+                            tag: 'img',
+                            src: '/consultation/getCaptcha'
+                        }
                     },
                     {
                         xtype: 'textfield',
                         itemId:'captchaField',
                         fieldLabel:'Captcha',
                         labelWidth: 50,
-                        margin: "5 0 5 0"
+                        margin: "5 0 5 0",
+                        listeners: {
+                            afterrender: function(field) {
+                                field.focus();
+                            }
+                        },
                     },
                     {
                         xtype: 'button',
